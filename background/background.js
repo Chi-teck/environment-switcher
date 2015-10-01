92a3dc15b205fb7e34f1bb0f3b2afb115549648f
Chi-teck <Chi-teck@users.noreply.github.com>
2015-10-01 10:53:02 +0500

    Cleanup.

--- background/background.js
+++ background/background.js
@@ -1,7 +1,6 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 
     if (changeInfo.status == 'loading') {
-        console.log(tab.url);
         chrome.pageAction.show(tabId);
     }
 
